ac961017583e55f388c993c534c7807e5c65bff0
Justyna <kowalkowska.just@gmail.com>
2021-11-16 23:36:30 +0100

    Write a program that calculates the sum of two real numbers.

---
+++ Addition/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Addition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 2.
+            // Write a program that calculates the sum of two real numbers.
+
+            Console.WriteLine("2. Addition.");
+
+            double x, y, sum;
+
+            Console.WriteLine("Enter the value of X: ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of Y: ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            sum = x + y;
+
+            Console.WriteLine("\nThe sum of {0} + {1} = {2}.", x, y, x+y);
+            Console.ReadKey();
+        }
+    }
+}
--- test_1/Program.cs
+++ test_1/Program.cs
@@ -1,6 +1,8 @@
-﻿// 1. Utworzenie programu, który wypisuje na ekranie powitanie,
-// czeka na reakcje użytkownika. Potem wypisuje "Koniec"
-// i znów czeka na reakcję użytkownika i następnie kończy działanie.
+﻿//
+// test_1
+//
+// Created by Justyna Kowalkowska on 06/11/2021.
+//
 
 
 using System;
@@ -11,37 +13,24 @@ namespace test_1
     {
         static void Main(string[] args)
         {
+
+            // 1.
+            // The creation of a program that prints a greeting to the screen waits
+            // for user responses. Then it prints "Finish", waits again for user input,
+            // and then exits.
+
+
             Console.WriteLine("What is your name?");
 
             string name;
             name = Console.ReadLine();
 
-            Console.WriteLine("Hello " + name);
+            Console.WriteLine("Hello " + name +".");
 
             Console.ReadKey(true);
-            Console.Write("Finish! \x4d");
+            Console.Write("Finish!");
             Console.ReadKey(true);
 
-            Console.Write("Podaj wartosc temperatury");
-            double temp = Convert.ToDouble(Console.ReadLine());
-
-            //double suma = temp + 3;
-
-            //Console.WriteLine("Suma wynosi {0}", suma);
-
-
-
-            //2. Zmienne i stałe
-
-            Console.WriteLine("2. Zmienne i stałe.");
-            double x, y, suma;
-            Console.Write("Podaj x: ");
-            x = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Podaj y: ");
-            y = Convert.ToDouble(Console.ReadLine());
-            suma = x + y;
-            Console.WriteLine("{0}+{1}={2}", x, y, suma);
-            Console.ReadKey();
         }
     }
 }
