2db83696219a5428e5907aedba639e15347f2d75
Justyna <kowalkowska.just@gmail.com>
2021-11-17 01:22:33 +0100

    Enumerated Type.

---
+++ EnumeratedType/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EnumeratedType
+{
+    class Program
+    {
+        // 4.
+        // Enumerated Type
+        // Definition of the enumeration.
+
+        enum Colors: byte { White, Yellow, Green, Orange, Red, Blue, Black, Brown};
+    
+        static void Main(string[] args)
+        {
+            
+        Console.WriteLine("My favorite color is: {0}", Colors.Red);
+        }
+    }
+}
